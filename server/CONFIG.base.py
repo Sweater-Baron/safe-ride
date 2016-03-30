8bd3870f6ff98a6480d9e32f0393a4fb02be69eb
Alex von Brandenfels <alex.brandenfels@gmail.com>
2016-03-30 15:12:10 -0700

    Initial commit

---
+++ server/CONFIG.base.py
@@ -0,0 +1,7 @@
+"""
+Configuration of the server.
+"""
+
+### Flask settings
+PORT=5000        # Probably want this to be 80 on production server
+DEBUG = True     # Set to False on production server
---
+++ server/safe-serve.py
@@ -0,0 +1,44 @@
+import flask
+from flask import render_template
+from flask import request
+from flask import url_for
+from flask import redirect
+
+import json
+import uuid
+import logging
+
+# Global stuff:
+import CONFIG
+
+app = flask.Flask(__name__)
+app.secret_key = str(uuid.uuid4())
+
+# Pages:
+
+@app.route("/")
+@app.route("/index")
+def index():
+  app.logger.debug("Index")
+  return flask.render_template('index.html')
+
+@app.route("/_create", methods=["POST"])
+def handle_create_request():
+    app.logger.debug("Recieved ride request")
+    fields = ["studentid", "phonenumber", "pickup", "dropoff", "time"]
+    user_request = {}
+    for thing in fields:
+        user_request[thing] = request.form[thing]
+    print(user_request)
+    return "Your request has been processed."
+
+@app.errorhandler(404)
+def page_not_found(error):
+    app.logger.debug("404")
+    return flask.render_template('page_not_found.html'), 404
+
+
+if __name__ == "__main__":
+    app.debug=CONFIG.DEBUG
+    app.logger.setLevel(logging.DEBUG)
+    app.run(port=CONFIG.PORT,host="0.0.0.0")
