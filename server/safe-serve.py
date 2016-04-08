6ec4b5c6dcc40b8ad6c2b663f6376e6ad5aa67ca
Alex von Brandenfels <alex.brandenfels@gmail.com>
2016-04-08 21:29:54 +0200

    Added JSON accepter

--- server/safe-serve.py
+++ server/safe-serve.py
@@ -31,6 +31,16 @@ def handle_create_request():
         user_request[thing] = request.form[thing]
     print(user_request)
     return "Your request has been processed."
+    
+@app.route("/_createFromApp")
+    studentid = request.args.get("attempt", type=str)
+    fields = ["name", "studentid", "phonenumber", "pickup", "dropoff", "time"]
+    user_request = {}
+    for thing in fields:
+        user_request[thing] = request.args.get(thing, type=str)
+    user_request["numberOfPassengers"] = request.args.get(
+        "numberOfPassengers", type=str)
+    print(user_request)
 
 @app.errorhandler(404)
 def page_not_found(error):
