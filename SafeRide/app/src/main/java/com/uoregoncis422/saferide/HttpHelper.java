76e7754585ac03f925096b638b3f04b15f4b6d95
Charlie Quinn <Charlie@128-223-223-71-wireless.uoregon.edu>
2016-04-21 11:06:46 -0700

    Android now requests using post instead of JSON in body

--- SafeRide/app/src/main/java/com/uoregoncis422/saferide/HttpHelper.java
+++ SafeRide/app/src/main/java/com/uoregoncis422/saferide/HttpHelper.java
@@ -3,11 +3,16 @@ package com.uoregoncis422.saferide;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -16,6 +21,10 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * Created by Charlie on 4/6/16.
@@ -29,19 +38,38 @@ public class HttpHelper {
         DB = database;
     }
 
-    public void uploadJSON(String jString){
-        request(jString);;
+    public void uploadJSON(JSONObject jObj){
+        String URLString = buildURLString(jObj);
+        request(URLString);;
     }
 
-    private void request(String jString){
-        new OtherThread().execute(jString);
+    private String buildURLString(JSONObject jObj){
+        String URLString = "?";
+        Iterator keys = jObj.keys();
+        try {
+            while (keys.hasNext()) {
+                String key = (String) keys.next();
+                URLString = URLString + URLEncoder.encode(key, "UTF-8") + "=" + URLEncoder.encode(jObj.getString(key), "UTF-8");
+                if(keys.hasNext()){
+                    URLString = URLString + "&";
+                }
+            }
+        }catch(Exception e){
+            Log.i("upload","problem with building URLString "+ e.toString());
+        }
+        return URLString;
+    }
+
+    private void request(String URLString){
+        new OtherThread().execute(URLString);
+        Log.i("upload","starting post thread");
     }
 
     private class OtherThread extends AsyncTask<String, Void, String> {
-        protected String doInBackground(String... jString) {
+        protected String doInBackground(String... URLString) {
                 String data = "";
                 try {
-                    URL url = new URL("http://ix.cs.uoregon.edu:6666/_createFromApp?="+jString[0]);
+                    URL url = new URL("http://ix.cs.uoregon.edu:6666/_createFromApp"+URLString[0]);
                     Log.i("upload", "URL is " + url.toString());
 
                     URLConnection conn = url.openConnection();
@@ -64,6 +92,50 @@ public class HttpHelper {
                     Log.i("upload", "error here "+ e.toString()+ " "+ e.getCause() );
                 }
                 return data;
+        }
+
+        protected void onPostExecute(String response) {
+            Log.i("upload",response);
+        }
+    }
+
+    private class PostRequestThread extends AsyncTask<JSONObject, Void, String> {
+        protected String doInBackground(JSONObject... jObj) {
+            String data = "";
+            try {
+                HttpClient client = new DefaultHttpClient();
+                HttpPost post = new HttpPost("http://ix.cs.uoregon.edu:6666/_createFromApp");
+
+                List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+
+                Iterator keys = jObj[0].keys();
+                while (keys.hasNext()) {
+                    String key = (String) keys.next();
+                    pairs.add(new BasicNameValuePair(key, jObj[0].getString(key)));
+                }
+
+                Log.i("upload","getting here");
+
+                post.setEntity(new UrlEncodedFormEntity(pairs));
+
+                HttpResponse response = client.execute(post);
+
+                HttpEntity entity = response.getEntity();
+                InputStream instream = entity.getContent();
+
+                BufferedReader br = new BufferedReader((new InputStreamReader(
+                        instream)));
+                StringBuffer b = new StringBuffer();
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    b.append(line);
+                }
+                data = b.toString(); //response from server to string
+
+            }catch (Exception e){
+                Log.i("upload", "error here "+ e.toString()+ " "+ e.getCause() );
+            }
+            return data;
         }
 
         protected void onPostExecute(String response) {
